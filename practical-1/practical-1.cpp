24cede59327a66865c28d58565339e4c53b8e12c
vansh-seth <111755254+vansh-seth@users.noreply.github.com>
2024-01-13 13:39:38 +0530

    Update practical-1.cpp

--- practical-1/practical-1.cpp
+++ practical-1/practical-1.cpp
@@ -1,88 +1,69 @@
 #include <iostream>
 using namespace std;
-class user
-{
-    string name;
-    float accno;
-    string type;
-    float bal;
-
-public:
-    void data() 
-    {
-        cout << "Enter name :\n";
-        cin >> name;
-        cout << "Enter Account number :\n";
-        cin >> accno;
-        cout << "Enter Account type :\n";
-        cin >> type;
-        cout << "Enter Balance :\n";
-        cin >> bal;
+class complex{
+    private :
+    float real, img;
+    public :
+    complex(){
+        real = 0;
+        img = 0;
     }
-    void deposit() 
-    {
-        cout << "Enter Amount to be deposited in account number " << accno << ":\n";
-        float am;
-        cin >> am;
-        bal += am;
-        cout << "Balance : " << bal << "\n";
-    }
-    void withdraw() 
-    {
-        cout << "Current Balance : " << bal << "\n";
-        cout << "Enter Amount to withdraw from account number " << accno << ":\n";
-        float am;
-        cin >> am;
-        bal -= am;
-        cout << "Balance : " << bal << "\n";
-    }
-    void dispdata() 
-    {
-        cout << "Name : " << name << "\n"
-             << "Balance : " << bal << "\n\n";
-    }
+    void inp();
+    void add(complex c1, complex c2);
+    void sub(complex c1, complex c2);
+    void prod(complex c1, complex c2);
+    void divi(complex c1, complex c2);
+    void disp();
 };
-
-int main()
-{
-    cout << "DataBase :\n"; 
-    user data[10];
-    for (int i = 0; i < 10; i++)
-    {
-        data[i].data();
-        cout<< "Enter new \n";
-    }
-
-    string tell;
-    cout << "Enter yes or no :\n";
-    cin >> tell;
-    while (tell == "yes")
-    {
-        cout << "Enter user Number :\n";
-        int num;
-        cin >> num;
-        cout << "Enter Operation you want to perform\n";
-        cout<< "1. deposit\n";
-        cout<< "2. withdraw\n";
-        cout<< "3. display\n";
-        int op;
-        cin >> op;
-        switch (op)
-        {
-
-        case 1:
-            data[num].deposit(); 
-            break;
-        case 2:
-            data[num].withdraw(); 
-            break;
-        case 3:
-            data[num].dispdata(); 
-            break;
-        default:
-            cout<<"invalid choice";
-        }
-        cout << "Want to continue enter yes :\n";
-        cin >> tell;
-    }
+void complex :: inp(){
+    cout << "enter real part : "; cin >> real;
+    cout << "enter imaginary part : "; cin >> img;
 }
+void complex :: add(complex c1, complex c2){
+    real = c1.real + c2.real;
+    img = c1.img + c2.img;
+}
+void complex :: sub(complex c1, complex c2){
+    real = c1.real - c2.real;
+    img = c1.img - c2.img;
+}
+void complex :: prod(complex c1, complex c2){
+    real = c1.real * c2.real - c1.img * c2.img;
+    img = c1.real * c2.img + c2.real * c1.img;
+}
+void complex :: divi(complex c1, complex c2){
+    real = (c1.real * c2.real + c1.img * c2.img)/(c2.real*c2.real + c2.img*c2.img);
+    img = (c2.real * c1.img - c1.real * c2.img)/(c2.real*c2.real + c2.img*c2.img);
+}
+void complex :: disp(){
+    cout << real << " + " << img << "i";
+}
+int main(){
+    complex c1,c2,c3,c4,c5,c6;
+    cout << "enter first complex number\n";
+    c1.inp();
+    cout << "enter second complex number\n";
+    c2.inp();
+    c3.add(c1,c2);
+    c4.sub(c1,c2);
+    c5.prod(c1,c2);
+    c6.divi(c1,c2);
+    cout << "\nAddition : "; c3.disp();
+    cout << "\nSubtraction : "; c4.disp();
+    cout << "\nMultiplication : "; c5.disp();
+    cout << "\nDivision : "; c6.disp();
+    return 0;
+}
+/* Output
+enter first complex number
+enter real part : 3
+enter imaginary part : 5
+enter second complex number
+enter real part : 2
+enter imaginary part : 7
+
+Addition : 5 + 12i
+Subtraction : 1 + -2i
+Multiplication : -29 + 31i
+Division : 0.773585 + -0.207547i
+*/
